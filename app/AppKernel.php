2a876e0a80470093ecd8d2d6924255c0d1700b3e
Stefan Koopmanschap <left@leftontheweb.com>
2014-01-21 15:35:17 +0100

    small but essential bugfix

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Ingewikkeld\LinktuesdayBundle\IngewikkeldLinkTuesdayBundle(),
+            new Ingewikkeld\LinkTuesdayBundle\IngewikkeldLinkTuesdayBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
