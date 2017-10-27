f0eb99a7d6b685635a8ef62787afc3b54b609538
Stefan Koopmanschap <stefan@ingewikkeld.net>
2017-10-27 10:15:52 +0200

    added twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,8 +7,15 @@ use Symfony\Component\HttpFoundation\Response;
 
 class HomeController implements Controller
 {
+    private $twig;
+
+    public function __construct(\Twig_Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     public function handleRequest(Request $request): Response
     {
-        return new Response('LinkTuesday');
+        return new Response($this->twig->render('home/index.twig'));
     }
 }
